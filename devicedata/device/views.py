1e12254b304e4bbad4457b8e8f85a8a250c2b738
sreeks87 <14892516+sreeks87@users.noreply.github.com>
2021-11-21 14:38:03 +0530

    fixed query issues

--- devicedata/device/views.py
+++ devicedata/device/views.py
@@ -1,7 +1,7 @@
 from django.db.models.aggregates import Avg
 from django.db import connection
 from django.shortcuts import render
-from django.db.models.functions import TruncMinute
+from django.utils import timezone
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -23,12 +23,8 @@ class DeviceView(APIView):
             return Response("error occurred ",status=status.HTTP_400_BAD_REQUEST)
      
     def get(self,request):
-        # items=Device.objects.raw('''SELECT customer_id,device_id,avg(reading) avg, 
-        # to_timestamp(floor((extract('epoch' from timestamp) / 300 )) * 300) 
-        # AT TIME ZONE 'UTC' as interval_alias
-        # FROM device_device GROUP BY customer_id,device_id,interval_alias,id
-        # ''')
         items=Device.objects.all()
+        agg_size=5
         if 'customer_id' in request.GET and request.GET['customer_id'] is not None: 
             cust_id=request.GET['customer_id'] 
             items=items.filter(customer_id=cust_id)
@@ -41,27 +37,34 @@ class DeviceView(APIView):
         if 'end_time' in request.GET and request.GET['end_time'] is not None:
             end_time=request.GET['end_time']
             items=items.filter(end_time__lt=end_time)
+        if 'agg_size' in request.GET and request.GET['agg_size'] is not None:
+            agg_size = request.GET['agg_size']
+        agg_size = int(agg_size)*60
+        agg_query="to_timestamp(FLOOR (EXTRACT (EPOCH FROM timestamp) / '{0}' )* {0})"
+        lower_bound_interval_aggregation={
+            "lower_bound": agg_query.format(agg_size)
+        }
+        print(lower_bound_interval_aggregation)
         # items.annotate(minute=TruncMinute('timestamp')).aggregate(Avg('reading'))
-        items = items.extra(select={'interval': "FLOOR (EXTRACT (EPOCH FROM timestamp) / '300' )* 300"}).values('interval','customer_id','device_id').annotate(value_avg=Avg('reading'))
-        print(connection.queries[-1])
-        print(items)
-        serializer=DeviceResponseSerializer(items,many=True)
-        return Response({'data':serializer.data},status=status.HTTP_200_OK)
+        items = items.extra(select=lower_bound_interval_aggregation).values('lower_bound','device_id','customer_id').annotate(value_avg=Avg('reading')).order_by('device_id','customer_id')
+        # print(connection.queries[-1])
+        each_data={}
+        each_reading={}
+        data=[]
+        prev_device=None
+        for i in items:
+            each_reading["timestamp"]=i["lower_bound"]
+            each_reading["reading"]=i["value_avg"]
+            if i["device_id"]!=prev_device:
+                readings=[]
+                each_data["device_id"]=i["device_id"]
+                each_data["customer_id"]=i["customer_id"]
+                readings.append(each_reading.copy())
+                each_data["readings"]=readings.copy()
+                data.append(each_data.copy())
+            else:
+                each_data["readings"].append(each_reading.copy())   
+            prev_device=i["device_id"]    
 
-
-
-
-# Dummy Request
-# [{
-# "timestamp": "2021-11-20T14:29:43+00:00",
-# "reading": 12.35,
-# "device_id": "23706ac3-88c4-45a4-9ca0-427d7f162cdb",
-# "customer_id": "2140c6cf-f513-489c-b49f-a8686582c664"
-# }]
-
-# {'interval': Decimal('1637418300'), 'customer_id': UUID('2140c6cf-f513-489c-b49f-a8686582c664'), 'device_id': UUID('23706ac3-88c4-45a4-9ca0-427d7f162cdb'), 'value_avg': 29.005}, 
-# {'interval': Decimal('1637418300'), 'customer_id': UUID('2140c6cf-f513-489c-b49f-a8686582c664'), 'device_id': UUID('d91c3e70-e642-430d-bbb8-9f190cf1df3c'), 'value_avg': 3050.1433333333334}, 
-# {'interval': Decimal('1637418300'), 'customer_id': UUID('c39f43c5-ce76-4b77-993a-f13dfdc5ece1'), 'device_id': UUID('d91c3e70-e642-430d-bbb8-9f190cf1df3c'), 'value_avg': 12.35}, 
-# {'interval': Decimal('1637418300'), 'customer_id': UUID('5866c3e9-89cf-4955-b7cc-b39762af5d0c'), 'device_id': UUID('1dae7442-a8ac-47d8-83f9-70672cdb0694'), 'value_avg': 12.35}, 
-# {'interval': Decimal('1637418600'), 'customer_id': UUID('2140c6cf-f513-489c-b49f-a8686582c664'), 'device_id': UUID('23706ac3-88c4-45a4-9ca0-427d7f162cdb'), 'value_avg': 347.3766666666666}, 
-# {'interval': Decimal('1637418000'), 'customer_id': UUID('2140c6cf-f513-489c-b49f-a8686582c664'), 'device_id': UUID('23706ac3-88c4-45a4-9ca0-427d7f162cdb'), 'value_avg': 1367.7199999999998}
+        serializer=DeviceResponseSerializer(data)
+        return Response({'data':data},status=status.HTTP_200_OK)
